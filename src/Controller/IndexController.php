a783eb68a30434b1e7c2edf52f06e60762e1b6f7
Akif Güldemir <akifguldemir@gmail.com>
2022-05-22 14:17:20 +0300

    ındexcontroller

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @return Response
+     */
+    public function index()
+    {
+
+        return new Response("Merhaba Akif");
+
+
+    }
+
+
+}
